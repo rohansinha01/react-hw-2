c5ef730cc45aa4b0116af588c59f2e887aa677ea
rohansinha01 <rohansinha01@gmail.com>
2024-01-29 22:40:12 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import movies from './movies';
 import Movie from './components/Movie'
+import Header from './components/Header';
 
 function App() {
   return (
+    
     <div className="App">
-      {
+      <Header />{
         movies.map(v => <Movie 
           name={v.name} 
           year={v.year} 
@@ -14,7 +16,7 @@ function App() {
           img={v.image}/>)
       }
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <div className="header">
+             <h1>Rohan Sinha's favorite Paul Thomas Anderson movies</h1>
+        </div>
+    )
+}
+
+export default Header
